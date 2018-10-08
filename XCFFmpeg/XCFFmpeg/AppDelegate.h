81a0ae7a6123d48594c3a7691c09d0dab7a09275
Apple <apple@AppledeMacBook-2.local>
2018-10-08 15:08:16 +0800

    first

---
+++ XCFFmpeg/XCFFmpeg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCFFmpeg
+//
+//  Created by Apple on 2018/10/8.
+//  Copyright © 2018年 XC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
